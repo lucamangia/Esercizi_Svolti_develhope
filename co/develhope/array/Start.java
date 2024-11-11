32ad7eeb63eb659e22e15bba168e1e84044066f3
luca.mangia <lukamangiamele@gmail.com>
2024-11-11 16:27:01 +0100

    Svolto il primo esercizio sugli Array.

---
+++ co/develhope/array/Start.java
@@ -0,0 +1,12 @@
+package co.develhope.array;
+
+public class Start {
+    public static void main(String[] args) {
+
+        String [] myColleagueNames = {" Giovanni, Emanuel, Gabriele, Stefano"};
+
+        for (int i = 0; i < myColleagueNames.length; i++){
+            System.out.println(myColleagueNames[i]);
+        }
+    }
+}
