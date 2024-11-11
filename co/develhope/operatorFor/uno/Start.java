0923c16c03ecaf0348d9fb5758b2ebaf1f2e51be
luca.mangia <lukamangiamele@gmail.com>
2024-11-11 16:41:10 +0100

    Svolto il primo esercizio sul for

---
+++ co/develhope/operatorFor/uno/Start.java
@@ -0,0 +1,17 @@
+package co.develhope.operatorFor.uno;
+
+public class Start {
+    public static void main(String[] args) {
+
+        String [] myColleagueNames = {"Giovanni", "Emanuel", "Gabriele", "Stefano"};
+        System.out.println(myColleagueNames.length);
+
+        for (int i = 0; i < myColleagueNames.length; i++){
+            System.out.println(myColleagueNames[i]);
+        }
+
+    }
+}
+
+
+
---
+++ out/production/Esercizi_Svolti_develhope/README.md
@@ -0,0 +1,2 @@
+# Esercizi_Svolti_develhope 
+# Questa repository contiene gli esercizi che verranno svolti dal 11/11/2024 in poi.
