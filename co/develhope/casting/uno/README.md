161739824e364f0da73d9f1514a441a6ce07603b
luca.mangia <lukamangiamele@gmail.com>
2024-11-11 18:56:59 +0100

    Svolto esercizio "Casting-1"

---
+++ co/develhope/casting/uno/README.md
@@ -0,0 +1,2 @@
+#Scrivere uno snippet di codice che dati 2 numeri 
+con la virgola (double) li sommi e li stampi senza virgola (int).
---
+++ co/develhope/casting/uno/Start.java
@@ -0,0 +1,10 @@
+package co.develhope.casting.uno;
+
+public class Start {
+    public static void main(String[] args) {
+        double a  = 2146.541D;
+        double b  = 5478.573D;
+        int somma = (int) (a+b);
+        System.out.println(somma);
+    }
+}
