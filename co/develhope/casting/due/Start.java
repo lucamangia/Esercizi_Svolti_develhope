198d7d38835c84d53db5c984e92f9e2e44ebb3f4
luca.mangia <lukamangiamele@gmail.com>
2024-11-11 19:09:09 +0100

    Svolto esercizio "Casting-2"

---
+++ co/develhope/casting/due/Start.java
@@ -0,0 +1,13 @@
+package co.develhope.casting.due;
+
+public class Start {
+    public static void main(String[] args) {
+
+        //Dichiarazione caratteri
+        char a = 'A';
+        char b = 'B';
+
+        int sommaValoriAscii = a+b;
+        System.out.println(sommaValoriAscii);
+    }
+}
