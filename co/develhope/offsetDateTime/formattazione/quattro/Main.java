a4ebcabf9fd8ebe0fff332a70e18509e25732e1b
luca.mangia <lukamangiamele@gmail.com>
2025-01-15 16:51:11 +0100

    esercizi esercizio date 4

---
+++ co/develhope/offsetDateTime/formattazione/quattro/Main.java
@@ -0,0 +1,25 @@
+package co.develhope.offsetDateTime.formattazione.quattro;
+
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+public class Main {
+    public class Date4 {
+        public static void main(String[] args) {
+            OffsetDateTime obj = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
+            obj = obj.plusYears(1);
+            obj = obj.minusMonths(1);
+            obj = obj.plusDays(7);
+
+            ZonedDateTime zonedDateTime = obj.toZonedDateTime();
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL).withLocale(Locale.getDefault());
+
+            System.out.println(dateTimeFormatter.format(zonedDateTime));
+
+}
+    }
+}
